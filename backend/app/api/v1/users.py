c4969f099cc1927f3c43dc628e0f2f33855ae199
Averakt <averakt@gmail.com>
2023-09-26 23:54:32 +0300

    Отключение проверки авторизованного пользователя

--- backend/app/api/v1/users.py
+++ backend/app/api/v1/users.py
@@ -67,8 +67,8 @@ def create_user():
 @bp.route('/v1/users/<int:id>', methods=['PUT'])
 @token_auth.login_required
 def update_user(id):
-    if token_auth.current_user().id != id:
-        abort(403)
+    # if token_auth.current_user().id != id:
+    #     abort(403)
     user = User.query.get_or_404(id)
     data = request.get_json() or {}
     if 'username' in data and data['username'] != user.username and \
